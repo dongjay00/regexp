31a718f871d65ea2148d45c3694346fea1881eac
dongjay00 <cjswodlehdgn@gmail.com>
2021-06-18 22:10:34 +0900

    method

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const str = `
+let str = `
 010-1234-5678
 thesecon@gmail.com
 https://www.omdbapi.com/?apikey=7035c60c&s=frozen
@@ -6,6 +6,7 @@ The quick brown fox jumps over the lazy dog.
 abbcccdddd
 `;
 
-// const regexp = new RegExp("the", "gi");
-const regexp = /the/gi;
-console.log(str.match(regexp));
+const regexp = /fox/gi;
+// console.log(regexp.test(str));
+str = str.replace(regexp, "AAA");
+console.log(str);
