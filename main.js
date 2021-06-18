a43978484a76ce05043bb73b22d1bc047eda4f74
dongjay00 <cjswodlehdgn@gmail.com>
2021-06-18 23:30:10 +0900

    pattern-1

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let str = `
-010-1234-5678.
+010-1234-5678
 thesecon@gmail.com
 https://www.omdbapi.com/?apikey=7035c60c&s=frozen
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
+http://localhost:1234
 `;
 
-console.log(str.match(/\.$/gim));
+console.log(str.match(/\b\w{2,3}\b/g));
