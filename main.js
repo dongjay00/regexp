b347cf68a8cc4c51d8d1e5bbe4cc147b4753d0f1
dongjay00 <cjswodlehdgn@gmail.com>
2021-06-18 22:39:06 +0900

    flag

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 let str = `
-010-1234-5678
+010-1234-5678.
 thesecon@gmail.com
 https://www.omdbapi.com/?apikey=7035c60c&s=frozen
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
 `;
 
-const regexp = /fox/gi;
-// console.log(regexp.test(str));
-str = str.replace(regexp, "AAA");
-console.log(str);
+console.log(str.match(/\.$/gim));
